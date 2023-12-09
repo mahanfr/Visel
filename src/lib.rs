6418bf3e36aad9a4ba2ce026b1ebd432f93020e6
mahanfarzaneh2000 <mahan.farzaneh@gmail.com>
2023-12-09 12:22:21 +0330

    parsing pixels and finding the decoding method

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,30 @@
+
 #[no_mangle]
-pub extern "C" fn dogpoop(left: usize, right: usize) -> usize {
-    left + right
+pub extern "C" fn _pixels_to_png(pixels: *const u32, width: usize, hight: usize) {
+}
+
+pub fn pixels_to_png(pixels: Vec<u32>) {
+    let mut is_alpha_separation = true;
+    let mut is_rgb_merging = false;
+    for pix in pixels {
+        let alpha: u8 = (pix >> 24) as u8 & 0xFF;
+        let blue: u8 = (pix >> 16) as u8 & 0xFF;
+        let green: u8 = (pix >> 8) as u8 & 0xFF;
+        let red:u8 = pix as u8 & 0xFF;
+        if alpha < 0xFF {
+            is_alpha_separation = false;
+        }
+        if (red & green & blue) == red {
+            is_rgb_merging = true;
+        }
+    }
+    if is_alpha_separation {
+        println!("Needs Alpha Seperation!");
+    }
+    if is_rgb_merging {
+        println!("Needs rgb Merging");
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = dogpoop(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
